c5adf754b9f5f88648250385ad22c73ab883fc67
gabrielrv228 <95095337+gabrielrv228@users.noreply.github.com>
2022-04-03 20:58:20 +0200

    Add files via upload

---
+++ test_gcloudstorage.py
@@ -0,0 +1,26 @@
+import os
+import sched, time
+from time import time, sleep
+
+def bucket_size():
+    os.system("gsutil du -s gs://acme-storage-prod/ |grep -E -o '[[:digit:]]+'")
+A = bucket_size()
+print (A)
+
+Size = int(0 if A is None else A)
+print(type(Size))
+Max_size = int(20971520)
+
+def bucket_size_check():
+    print("hola")
+    if Size > Max_size:
+        os.system("gsutil rm -r gs://acme-storage-prod")
+    else:
+        print('es menor de 20mb')  
+
+bucket_size_check()
+
+
+while True:
+    sleep(600 - time() % 600)
+    bucket_size_check()
